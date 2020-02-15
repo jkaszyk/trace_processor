042498a8748e78efd6114177c0c6e45062edf840
kuba <kubakaszyk@gmail.com>
2020-02-15 21:56:14 +0000

    old uncommited changes

--- process.py
+++ process.py
@@ -1,6 +1,10 @@
 ''' 
 TO DO:
  - Add position index to traces, e.g. using instruction count.
+ - Find nearest after allocating threads to warps, i.e. remove from working set (naive approach)
+ - Try clustering addresses (knn/k-means clustering)
+ - Zero-extend vector & calculate distance
+ - Implement metric for irregular workloads - distance doesn't really work, because one more/fewer access will dominate metric
 '''
 import sys
 import os
@@ -47,21 +51,36 @@ with open(trace_file) as csv_file:
         else:
             mem_list[gtid] = [baseaddr]
 
-# Calculate distances between traces
+# Standardize length
+max_length_mem = 0
+max_length_read = 0
+max_length_write = 0
+
+for gtid in mem_list:
+    l = len(mem_list[gtid])
+    if l > max_length_mem:
+        max_length_mem = l
+        
+for gtid in mem_list:
+    l = len(mem_list[gtid])
+    if l < max_length_mem:
+        mem_list[gtid].extend(np.zeros(max_length_mem - l))
+        
+# Calculate total distances between thread memory traces
 distance_lists = {}
-for k in mem_list:
+for gtid_1 in mem_list:
     distance_list = {}
-    for j in mem_list:
-        if k != j:
-            dist = np.abs(np.array(j) - np.array(k))
+    for gtid_2 in mem_list:
+        if gtid_1 != gtid_2:
+            dist = np.sum(np.abs(np.array(mem_list[gtid_2]) - np.array(mem_list[gtid_1])))
             if dist in distance_list.keys():
-                distance_list[dist].append(j)
+                distance_list[dist].append(gtid_2)
             else:
-                distance_list[dist] = [j]
-    if k in distance_lists:
-        distance_lists[k].append(distance_list)
+                distance_list[dist] = [gtid_2]
+    if gtid_1 in distance_lists:
+        distance_lists[gtid_1].append(distance_list)
     else:
-        distance_lists[k] = [distance_list]
+        distance_lists[gtid_1] = [distance_list]
 
 # Print stats
 num_threads = len(distance_lists)
@@ -80,29 +99,33 @@ print("Max write addr: " + str(hex(max_write_addr)))
 print("Write address space size: " + str(hex(write_addr_space_size)))
 
 # Print distances
-for k in distance_lists:
+for gtid_1 in distance_lists:
     i = 0
-    for distance in distance_lists[k]:
+    for distance in distance_lists[gtid_1]:
         if i < max_group_size - 1:
             for dist in sorted(distance.keys()):
-                for j in distance[dist]:
+                for gtid_2 in distance[dist]:
                     i += 1
                     if i <= max_group_size - 1:
-                        print(str(k) + ' ' + str(j) + ' ' + str(dist))
+                        print(str(gtid_1) + ' ' + str(gtid_2) + ' ' + str(dist))
         else:
             break
 
 # Calculate direction of memory accesses as a function of thread id
+# Number shows direction between current and next thread
 # E.g. Thread 0 accesses addr 0x0, thread 1 accesses addr 0x4, then this is increasing
-# E.g. Thread 0 accesses addr 0x10, therad 1 accesses addr 0xc, then this is decreasing
+# E.g. Thread 0 accesses addr 0x10, thread 1 accesses addr 0xc, then this is decreasing
 
 mem_list_keys = list(mem_list.keys())
-dir_vec = []
-print("Direction")
+sum_dir_vec = []
+avg_dir_vec = []
+print("Sum Direction")
 for i in range(0,len(mem_list_keys)-1):
     j = mem_list[mem_list_keys[i]]
     k = mem_list[mem_list_keys[i+1]]
     dir = np.sum(np.abs(np.array(j) - np.array(k)))
-    dir_vec.append(dir)
-
-print(dir_vec)
+    sum_dir_vec.append(dir)
+    avg_dir_vec.append(dir*1.0/len(np.array(j)))
+print(sum_dir_vec)
+print("Average Direction")
+print(avg_dir_vec)
